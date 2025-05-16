297b4bc8fdbb204c3796be20ec8b7ab1f9590992
Sahid Sousa <sahid.sousa@bol.com.br>
2025-05-16 00:30:33 -0300

    Ajuste:  - Na chamada do metodo Startup::main

--- src/main/java/br/com/rest/rest/Startup.java
+++ src/main/java/br/com/rest/rest/Startup.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Startup {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestWithSpringBootApplication.class, args);
+        SpringApplication.run(Startup.class, args);
     }
 
 }
